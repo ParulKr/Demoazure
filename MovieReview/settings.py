eb76069a4c006016e16be95110cc9d4769fcf117
ParulKr <119673996+ParulKr@users.noreply.github.com>
2023-01-18 12:32:10 +0530

    Update settings.py

--- MovieReview/settings.py
+++ MovieReview/settings.py
@@ -123,3 +123,4 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['https://princecons.azurewebsites.net', '127.0.0.1']
